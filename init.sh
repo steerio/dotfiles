f6b3ce354ce546f46252e195f0f9d085c35b47c4
Roland Venesz <roland.venesz@wopata.com>
2014-07-11 12:27:02 +0200

    Init fix: do not use built-in which

--- init.sh
+++ init.sh
@@ -40,7 +40,7 @@ fi
 
 # Heroku completion
 
-if which heroku >/dev/null; then
+if /usr/bin/which heroku >/dev/null; then
   echo 'Heroku client present, scraping CLI for completion'
   ruby scrape-heroku.rb >.zsh/comp/_heroku
 fi
